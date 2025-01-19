b8ad1e145988738a6db7467848827f9275110b9e
Somjit Nag <somjitnag192@gmail.com>
2025-01-19 21:12:07 +0530

    property reloaded via actuator

--- restapi/src/main/java/example/dataretention/restapi/config/PurgeDetails.java
+++ restapi/src/main/java/example/dataretention/restapi/config/PurgeDetails.java
@@ -5,6 +5,7 @@ import example.dataretention.shared.domain.Table;
 import example.dataretention.shared.domain.Tenant;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @Data
 @Service
 @ConfigurationProperties("app")
+@RefreshScope
 public class PurgeDetails {
     private List<Tenant> tenants;
     private List<Table> tables;
