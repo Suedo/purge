8b63341ed5fa3fb4078fbcf862e2b8a13ebc916b
Somjit Nag <somjitnag192@gmail.com>
2025-01-19 20:51:30 +0530

    distributed locking with redis works

--- restapi/src/main/java/example/dataretention/restapi/MultiTenantRecordCleanerService.java
+++ restapi/src/main/java/example/dataretention/restapi/MultiTenantRecordCleanerService.java
@@ -2,6 +2,8 @@ package example.dataretention.restapi;
 
 
 import example.dataretention.restapi.config.MultiTenantProperties;
+import example.dataretention.restapi.config.PurgeDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,20 +16,24 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class MultiTenantRecordCleanerService {
     private final RedissonClient redissonClient;
     private final JdbcTemplate jdbcTemplate;
     private final MultiTenantProperties multiTenantProperties;
+    private final PurgeDetails purgeDetails;
 
-    public MultiTenantRecordCleanerService(RedissonClient redissonClient, JdbcTemplate jdbcTemplate, MultiTenantProperties multiTenantProperties) {
+    public MultiTenantRecordCleanerService(RedissonClient redissonClient, JdbcTemplate jdbcTemplate, MultiTenantProperties multiTenantProperties, PurgeDetails purgeDetails) {
         this.redissonClient = redissonClient;
         this.jdbcTemplate = jdbcTemplate;
         this.multiTenantProperties = multiTenantProperties;
+        this.purgeDetails = purgeDetails;
     }
 
-    @Scheduled(fixedRateString = "#{@multiTenantProperties.schedule.fixedRate.toMillis()}")
+    @Scheduled(cron = "0 */2 * * * *") // Every 5 minutes
     public void cleanOldRecords() {
+        log.info("CRON triggered");
         // New logic: Dynamic list of clients and tables
         List<String> clients = List.of("INFOSYS", "TCS");
         List<String> tables = List.of("FleetData", "EmployeeRequests");
@@ -47,28 +53,46 @@ public class MultiTenantRecordCleanerService {
     @Transactional
     public void cleanTableTransactional(String tableName) {
         String lockKey = "lock:" + tableName;
+        log.info("Trying to delete table: {}, using lock: {}", tableName, lockKey);
+
         RLock lock = redissonClient.getLock(lockKey);
+
+        long startTime = System.currentTimeMillis(); // Start time for measuring lock wait
 
         try {
             if (lock.tryLock(10, 60, TimeUnit.SECONDS)) {
-                deleteOldRecords(tableName);
+                long acquiredTime = System.currentTimeMillis(); // Time when lock was acquired
+                log.info("Lock acquired for table: {} after waiting {} ms", tableName, (acquiredTime - startTime));
+
+                var rowsDeleted = deleteOldRecords(tableName);
+                log.info("Deleted {} rows from {}", rowsDeleted, tableName);
             } else {
+                log.warn("Could not acquire lock for table: {} after waiting 10 seconds", tableName);
                 throw new RuntimeException("Could not acquire lock for table: " + tableName);
             }
         } catch (InterruptedException e) {
+            log.error("Interrupted while waiting for lock on table: {}", tableName, e);
             throw new RuntimeException(e);
         } finally {
-            if (lock.isHeldByCurrentThread()) lock.unlock();
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+                log.info("Lock released for table: {}", tableName);
+            }
         }
     }
 
-    private void deleteOldRecords(String tableName) {
+
+    private int deleteOldRecords(String tableName) throws InterruptedException {
         String deleteQuery = """
                     DELETE FROM %s 
-                    WHERE created_at < NOW() - INTERVAL %d DAY
+                    WHERE reservation_date < NOW() - INTERVAL %d DAY
                     AND status NOT IN ('In Progress', 'Approved');
                 """.formatted(tableName, multiTenantProperties.getDeletion().interval().getDays());
 
-        jdbcTemplate.update(deleteQuery);
+        log.info("deleteOldRecords: deleteQuery: {}", deleteQuery);
+
+        var count = jdbcTemplate.update(deleteQuery);
+        Thread.sleep(2000);
+        return count;
     }
 }
--- restapi/src/main/java/example/dataretention/restapi/PurgeApplication.java
+++ restapi/src/main/java/example/dataretention/restapi/PurgeApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = {"example.dataretention.restapi", "example.dataretention.shared"})
 @EnableConfigurationProperties({PurgeDetails.class})
+@EnableScheduling
 public class PurgeApplication {
 
     public static void main(String[] args) {
